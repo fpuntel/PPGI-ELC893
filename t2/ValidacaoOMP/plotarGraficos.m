d35487583e3144782102cd4ed3c8601c5e5f5333
fpuntel <fernandopuntel@gmail.com>
2016-09-20 21:54:56 -0300

    Trabalho 2

--- t2/ValidacaoOMP/plotarGraficos.m
+++ t2/ValidacaoOMP/plotarGraficos.m
@@ -1,9 +1,11 @@
-# Codigo ira plotar dados resultantes do codigo fortran
-# com e sem paralelizacao
+# Funcoes do codigo:
+# - plotar dados resultantes do codigo fortran com e sem paralelismo;
+# - calculo distancia euclidiana entre os vetores resultantes;
 # Autor: Fernando Emilio Puntel
+# PPGI - Programa de pos graduacao e Informatica UFSM
+# Professora Andrea Charao
 
-# Plot sem paralelizacao
-
+# Carregar matrizes sem paralelismo
  load uGl.txt;
  who;            
  uGl;
@@ -12,36 +14,60 @@ load vGl.txt;
 who;            
 vGl;
 
-# Com paralelizacao
+# Carregar matrizes com paralelismo
  load uGlParalelizado.txt;
  who;            
  uGlParalelizado;
-
-# Calculo norm  
+ 
 load vGlParalelizado.txt;
 who;            
 vGlParalelizado;
 
-"uGl"
+# Norm 
+disp("uGl - Norm l^2")
  norm(uGl-uGlParalelizado)
 
- "vGl"
-  norm(vGl-vGlParalelizado)
+disp("vGl - Norm l^2")
+norm(vGl-vGlParalelizado)
+
+# Norm manual (Distância Euclidiana)
+tamanho=159200;
+ind=1;
+soma=0;
   
-# Plots
-#figure(1)
-#clf ()
-#plot(uGl)
-#title('uGl sem paralelizacao | Matriz 100 x 100')
+# uGl 
+for ind =1:tamanho
+   sub = uGl(ind) -uGlParalelizado(ind);
+   pot = sub * sub;
+   soma = soma + pot;
+endfor
+  
+ normUgl = sqrt(soma)
+  
+# vGl 
+soma = 0;
+for ind =1:tamanho
+   sub = vGl(ind) - vGlParalelizado(ind);
+   pot = sub * sub;
+   soma = soma + pot;
+endfor
+normVgl = sqrt(soma)
 
-#figure(2)
-#plot(vGl)
-#title('vGl sem paralelizacao | Matriz 100 x 100')
 
- #figure(3)
- #plot(uGlParalelizado)
- #title('uGl com paralelizacao | Matriz 100 x 100')
+# Plots dos graficos
+figure(1)
+clf ()
+plot(uGl)
+title('uGl sem paralelizacao | Matriz 100 x 100')
+
+figure(2)
+plot(vGl)
+title('vGl sem paralelizacao | Matriz 100 x 100')
+
+figure(3)
+plot(uGlParalelizado)
+title('uGl com paralelizacao | Matriz 100 x 100')
  
-#figure(4)
-#plot(vGlParalelizado)
-#title('vGl com paralelizacao | Matriz 100 x 100')
+figure(4)
+plot(vGlParalelizado)
+title('vGl com paralelizacao | Matriz 100 x 100')
